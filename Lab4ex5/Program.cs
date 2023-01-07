099f216f414ea630d3d422c603f56c62d4a73902
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2023-01-07 23:00:48 +0200

    Add project files.

---
+++ Lab4ex5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab4ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti o functie recursiva care va calcula suma numerelor de la 1 pana la n, apelati-o si afisati-i rezultatul.*/
+
+            Console.WriteLine("Introduceti n= ");
+            int n = int.Parse(Console.ReadLine());
+
+            int rezultat = CalculeazaSumaNumerelor(1, n);
+            Console.WriteLine("Suma numerelor este: "+ rezultat);
+
+        }
+
+        static int CalculeazaSumaNumerelor(int x, int suma)
+        {
+            if (x == suma)
+                return suma;
+            return suma + CalculeazaSumaNumerelor(x, suma - 1);
+        }
+    }
+
+}
